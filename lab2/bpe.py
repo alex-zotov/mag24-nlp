4cf36dc730d9f4211ab8ec90c430c96c75b1b85b
Alexander Zotov <alex.m.zotov@gmail.com>
2025-12-15 01:57:42 +0500

    BPE encode decode  save load

--- lab2/bpe.py
+++ lab2/bpe.py
@@ -4,8 +4,11 @@
 Всё зависит от размера корпуса на котором учим токенизатор и размера словаря.
 '''
 
+from typing import List, Dict
+import dill
 
-class BBE():
+
+class BPE():
     '''
     Byte-Pair Encoding
     '''
@@ -15,9 +18,9 @@ class BBE():
         self.vocab_size: int = vocab_size
 
         # запомним как преобразовать число в токен
-        self.id2token: list[str] = []
+        self.id2token: Dict[int, str] = {}
         # и как преобразовать токен в число
-        self.token2id: dict[str, int] = {}
+        self.token2id: Dict[str, int] = {}
 
     def fit(self, text: str):
         '''
@@ -39,10 +42,9 @@ class BBE():
         # если два токена имеют одинаковую частоту,
         # то токен, который встретился раньше в тексте будет иметь порядок в словаре id2token меньше
 
-        self.id2token = [c for c in set(text)]
-        self.id2token.sort()
+        self.id2token = {id: c for id, c in enumerate(sorted(set(text)))}
 
-        self.token2id = {c: i for c, i in enumerate(self.id2token)}
+        self.token2id = {c: id for id, c in self.id2token.items()}
 
         assert (len(self.id2token) < self.vocab_size
                 ), 'слишком маленький размер словаря'
@@ -52,7 +54,7 @@ class BBE():
 
         # корпус разбит на токены
         # храним начало токена в корпусе и его длину
-        tokensLen: list[int] = [1 for _ in text]
+        tokensLen: List[int] = [1 for _ in text]
 
         # бежим по токенам корпуса пытаемся объединить токен с рядом стоящим справа
         # у объединения считаем частоту (частота объединения не больше частоты токена)
@@ -66,7 +68,7 @@ class BBE():
 
             # если каждый токен объединить с соседом справа
             # посчитаем вхождения таких пар
-            pairCnt: dict[str, int] = {}
+            pairCnt: Dict[str, int] = {}
             while i_token_next < len(text):
                 i_token_next2 = i_token_next + tokensLen[i_token_next]
 
@@ -99,5 +101,66 @@ class BBE():
                     i_token_next = i_token_next2
 
             # добавляем максимальную пару в decoder
-            self.token2id[max_pair] = len(self.id2token)
-            self.id2token.append(max_pair)
+            id: int = len(self.id2token)
+            self.id2token[id] = max_pair
+            self.token2id[max_pair] = id
+
+    def encode(self, text: str) -> List[int]:
+        '''
+        закодировать строку text обученным BPE
+        '''
+
+        if text == '':
+            return []
+
+        '''
+        для текущего символа строки выбираем все токены,
+        которые можно применить с текущего символа
+        применяем самый длинный
+        '''
+
+        embending: List[int] = []
+        i: int = 0
+        while i < len(text):
+            max_token: str = ''
+            for token in self.id2token.values():
+                if text[i:i+len(token)] == token and len(token) > len(max_token):
+                    max_token = token
+            embending.append(self.token2id[max_token])
+
+            i += len(max_token)
+
+        return embending
+
+    def decode(self, token_ids: List[int]) -> str:
+        '''
+        декодировать эмбендинг
+        '''
+
+        text: str = ''
+
+        for id in token_ids:
+            text += self.id2token[id]
+
+        return text
+
+    def save(self, filename: str):
+        '''
+        сохранить токенайзер в файл
+        '''
+
+        with open(filename, 'wb') as f:
+            dill.dump(self, f)
+        print(f"Объект сохранён в {filename}")
+
+    @classmethod
+    def load(cls, filename: str):
+        '''
+        загрузить токенайзер из файла
+        '''
+
+        with open(filename, 'rb') as f:
+            obj = dill.load(f)
+
+        print(f"Объект загружен из {filename}")
+        return obj
